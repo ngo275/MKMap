8aeb48ed2d6c6fc1ed992f9750d0defb0601a5ea
Shuichi Nagao <ngo38114@gmail.com>
2016-12-10 23:08:47 +0900

    Initial Commit

---
+++ MKMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MKMap
+//
+//  Created by ShuichiNagao on 2016/12/10.
+//  Copyright © 2016 ShuichiNagao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
